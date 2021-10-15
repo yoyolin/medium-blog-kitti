93cf0b873ae41a32fad55b67eb431ae75c4079a2
yiyoulin <yiyoulin@microsoft.com>
2021-10-14 18:50:43 -0700

    input analyzer done - pending input visualization & DontCare, hard example cleanup

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -2,15 +2,16 @@ import os
 import json
 import time
 import argparse
-from shutil import copy2
+import random
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from shutil import copy2
 from typing import List, Dict
 from functools import partial
-from PIL import Image, ImageDraw, ImageFont
 import seaborn as sns
 import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
 from IPython.display import display, clear_output
 
 def load_jsonl(json_fname: str) -> List[Dict]:
@@ -93,6 +94,49 @@ class JsonParser:
         df = pd.DataFrame(data = labels)
         return df
 
+class InputVisualizerOD:
+
+    def __init__(self, labels = None):
+        self.font = ImageFont.truetype("./arial.ttf", size = 20)
+        
+        self.label_to_color = dict()
+        if labels is not None:
+            self.get_distinct_colors(labels)
+            
+    def get_color(self, label):
+        if label in self.label_to_color:
+            return self.label_to_color[label]
+        else:
+            color = np.random.choice(range(256), size=3)
+            self.label_to_color[label] = color
+            return color
+    
+    def get_distinct_colors(self, labels):
+        # a list of RGB tuples
+        colors = sns.color_palette('husl', 
+                                   n_colors = len(labels))  
+
+        for label, color in zip(labels, colors):
+            self.label_to_color[label] = tuple([int(c* 255) for c in color])
+    
+    def visualize(self, image_fname, image_df):
+        image = Image.open(image_fname)
+        draw = ImageDraw.Draw(image)
+
+        for _, bbox in image_df.iterrows():
+            color = self.get_color(bbox["label"])
+            draw.rectangle((bbox["topX"], 
+                            bbox["topY"], 
+                            bbox["bottomX"], 
+                            bbox["bottomY"]), 
+                            outline = color, 
+                            width = 5)
+            draw.text((bbox["topX"], bbox["topY"] - 20),
+                       bbox["label"], 
+                       align ="left",
+                       font = self.font,
+                       fill = color)
+        display(image)
 
 class InputAnalyzer:
     def __init__(self, task_type = ["OD", "MC", "ML", "SEG"]):
@@ -104,7 +148,7 @@ class InputAnalyzer:
     def add_file(self, json_fname: str, train_type = ["train", "validation", "test"]):
         df = self.json_parser.parse(json_fname)
         df["train_type"] = train_type
-        print(f"Added {train_type} dataframewith shape", df.shape)
+        print(f"Added {train_type} dataframe with shape", df.shape)
         self.dataframes.append(df)
     
     def aggregate_image_dfs(self):
@@ -128,17 +172,17 @@ class InputAnalyzer:
                 ))
 
     ######### Resolution Analysis ##########
-    def image_dim_plot(self):
+    def image_dimension_plot(self):
+        print("Creating image dimension plot")
         plt.figure(figsize=(10, 6))
-        ax = sns.kdeplot(data = self.image_df, x = "width", y = "height", 
-                         cmap="Reds", shade=True, bw_adjust=.5).\
+        ax = sns.kdeplot(data = self.image_df, x = "width", y = "height",
+                          cmap = "Reds", shade=True, bw_adjust=.5).\
                          set(title = "Height& Width 2D Plot")
         plt.xticks(rotation=45)
         plt.show()
         
-    def image_dim_analysis(self):    
+    def image_dimension_analysis(self):    
         print("Analyzing images dimension across train_type:")
-        self.image_dim_plot()
         display(self.image_df.aggregate(
             {"height": ["min", "max", "median"], 
              "width":  ["min", "max", "median"]}))
@@ -162,11 +206,14 @@ class InputAnalyzer:
                     series = image_df_with_full_labels["label"].value_counts(),
                     total = len(self.image_df)
         ))
+
         
 class InputAnalyzerOD (InputAnalyzer):
     def __init__(self):
         super().__init__(task_type = "OD")
-    
+        self.visualizer = InputVisualizerOD()
+        random.seed(100)
+        
     def aggregate_dfs(self):
         super().aggregate_image_dfs()
         
@@ -174,17 +221,24 @@ class InputAnalyzerOD (InputAnalyzer):
         self.bbox_df = pd.concat(self.dataframes, axis = 0)
         print("bbox_df",)
         display(self.bbox_df.head())
-
+        
+        # setups for visualizer
+        self.visualizer.get_distinct_colors(self.bbox_df["label"].unique())
+        
+        # setups for size column
         def get_size(row):
-            size = (row["bottomY"] - row["topY"])* (row["bottomX"] - row["topX"])
+            height, width = (row["bottomY"] - row["topY"]), (row["bottomX"] - row["topX"])
+            size = height*width 
             if size < 32 * 32:
-                return "small (<32x32)"
+                size = "small (<32x32)"
             elif size > 96 * 96:
-                return "large (>96x96)"
+                size = "large (>96x96)"
             else:
-                return "medium"
-        self.bbox_df["size"] = self.bbox_df.apply(get_size, axis = 1)
-
+                size = "medium"
+            return height, width, size
+        self.bbox_df[["bbox_height", "bbox_width", "bbox_size"]] = \
+                            self.bbox_df.apply(get_size, axis = 1, result_type='expand')    
+        
     def bbox_count_analysis(self):
         print("Total bounding boxes: ", len(self.bbox_df))
         print("Analyzing bboxes per train type:")
@@ -208,29 +262,50 @@ class InputAnalyzerOD (InputAnalyzer):
                         agg(["min", "max", "median"]))
     
     def bbox_size_analysis(self):
-        
         print("Analyzing size per train type")
         display(self.bbox_df.pivot_table(
                      index = "train_type", 
-                     columns = "size", 
+                     columns = "bbox_size", 
                      values = "fname", 
                      aggfunc = "count").\
-                         fillna(0))
-        
-        
-    def bbox_label_analysis(self):
-        display(get_percentage_df(
-                    series = self.bbox_df["label"].value_counts(),
-                    total = len(self.bbox_df)
-                ))
-        
+                     fillna(0))
+       
         print("Analyzing size per label")
         display(self.bbox_df.pivot_table(
                                          index = "label", 
-                                         columns = "size", 
+                                         columns = "bbox_size", 
                                          values = "fname", 
                                          aggfunc = "count").\
                              fillna(0))
+    def bbox_dimension_plot(): 
+        print("Creating bounding box dimension plot")
+        g = sns.FacetGrid(self.bbox_df, 
+                          col="label", col_wrap = 3,
+                          sharex = False, sharey = False
+                         )
+
+        g.map(sns.kdeplot, "bbox_width", "bbox_height", 
+                           shade = True, bw_adjust=.5)
         
+        
+    def bbox_label_analysis(self):
+        print("Analyzing bbox count per label")
+
+        display(get_percentage_df(
+                    series = self.bbox_df["label"].value_counts(),
+                    total = len(self.bbox_df)
+                )) 
     
+    def visualize_by_fname(self, fname):
+        print(fname)
+        self.visualizer.visualize(image_fname = fname,
+                                  image_df = self.bbox_df[self.bbox_df["fname"] == fname])
     
+    def random_visualize(self, k = 1, condition = dict()):
+        sub_df = self.bbox_df
+        for key, val in condition.items():
+            sub_df = sub_df[sub_df[key] == val]
+        fnames = random.choices(sub_df["fname"].unique(), k = k)
+        for i, fname in enumerate(fnames):
+            print(i,)
+            self.visualize_by_fname(fname)
